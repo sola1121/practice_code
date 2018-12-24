3f8d2af681f06c520c620b72ba22c4f4a38db84a
sola1121 <sola1121@foxmail.com>
2018-12-24 18:01:57 +0800

    屏障同步进程Barrier

--- python3/python并行编程手册/ch03/P78_同步进程之一_Barrier.py
+++ python3/python并行编程手册/ch03/P78_同步进程之一_Barrier.py
@@ -40,5 +40,5 @@ if __name__ == "__main__":
 
     Process(name="p1 - test_with_barrier", target=test_with_barrier, args=(synchronizer, serializer)).start()
     Process(name="p2 - test_with_barrier", target=test_with_barrier, args=(synchronizer, serializer)).start()
-    Process(name="p3 - test_without_barrier", target=test_with_barrier).start()
-    Process(name="p4 - test_without_barrier", target=test_with_barrier).start()
+    Process(name="p3 - test_without_barrier", target=test_without_barrier).start()
+    Process(name="p4 - test_without_barrier", target=test_without_barrier).start()
