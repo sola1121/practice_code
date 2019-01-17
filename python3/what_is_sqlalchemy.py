361adff9106033ab28bc4e9b482dc233fecd9f49
sola1121 <sola1121@foxmail.com>
2019-01-17 18:59:29 +0800

    使用sqlalchemy

--- python3/what_is_sqlalchemy.py
+++ python3/what_is_sqlalchemy.py
@@ -2,9 +2,29 @@
 # https://docs.sqlalchemy.org/en/rel_1_2/orm/tutorial.html
 
 import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import create_session
+from sqlalchemy import MetaData, Table, String, Integer
+
 
 db_engine = sqlalchemy.create_engine("mysql+pymysql://leimilia:leimilia@localhost/employees?charset=utf8mb4")
 
+# 普通的pymysql式的使用
 results = db_engine.execute("select * from departments limit 10")
 for result in results.fetchall():
     print(result)
+
+
+Model = declarative_base()
+metadata = MetaData(bind=db_engine)
+
+class Departments(Model):
+    """映射已有的数据库，传入三个参数：数据库名称、metadata（绑定接口）、autoload=True"""
+    __table__ = Table("departments", metadata, autoload=True)
+
+# 创建事务(会话)
+session = create_session(bind=db_engine)
+results = session.query(Departments).all()
+
+for result in results:
+    print("部门号: ", result.dept_no, "   部门名: ", result.dept_name)
